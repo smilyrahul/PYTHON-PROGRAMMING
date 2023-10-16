766f714bae4fd053d86559714c00585988db5c66
Rahul raj <rahulraj12777@gmail.com>
2023-10-17 03:46:30 +0530

    files

---
+++ Chp1_5strings.py
@@ -0,0 +1,13 @@
+name = "--you are"
+name1= '----cute'
+#--------------------for multiple line string excution ---------------- 
+    #-------------- ''' ''''(<-- triple single coma use)--------
+print( name)
+print( name1)
+mulline = ''' --- Hii 
+ i am rahul raj 
+ if you talk , i talk
+        & 
+ you quite , i quite:
+ ----'''
+print(mulline)
